d76b12c4e47926dd0f5a5c44d2783e7874f93789
David <okononfuadavid@gmail.com>
2020-11-21 12:41:24 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+//Initialize Swiper 
+
+  var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 'auto',
+    spaceBetween: 40,
+    centeredSlides: true,
+    grabCursor: true,
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  //   navigation: {
+  //     nextEl: '.swiper-button-next',
+  //     prevEl: '.swiper-button-prev',
+  //   },
+  });
